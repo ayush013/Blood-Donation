652f7c2ee92d3288486724e62d521154b3c3d67e
meghna300 <guptameghna300@gmail.com>
2018-02-13 16:14:39 +0530

    added sweet alerts

--- register_index.php
+++ register_index.php
@@ -7,12 +7,12 @@
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+     <link rel="stylesheet" href="assets/css/style.css">
 	<script type="text/javascript" src="assets/js/jquery-3.3.1.js"></script>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 	<script type="text/javascript" src="assets/js/validations.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	<link rel="stylesheet" href="assets/css/style.css">
+	
 </head>
 <body>
 	<style type="text/css">
